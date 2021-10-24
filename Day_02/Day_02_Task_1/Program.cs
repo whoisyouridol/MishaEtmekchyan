724313ae128767d08c60d2b6afe15671fcd5051b
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-24 09:07:38 +0400

    finish task_1

---
+++ Day_02/Day_02_Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_02_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //დაწერეთ კონსოლ აპლიკაცია სადაც მომხმარებელს ექნება საშუალება შეიტანოს მთელი
+            //რიცხვი, პროგრამამ უნდა გაარკვიოს ეს რიცხვი ლუწია თუ კენტი და იმის მიხედვით
+            //დაბეჭდოს შესაბამისი შეტყობინება.
+            Console.WriteLine("Enter integer number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number %2 == 0)     
+                Console.WriteLine("Entered number " + number + " is prime");
+           
+            else
+                Console.WriteLine("Entered number " + number + " is odd");
+
+        }
+}
+}
