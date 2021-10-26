8ef298dbec22f7ac802bebd883cb30f46fa663da
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-26 20:08:10 +0400

    finish task_2

---
+++ Day_03/Day_03_Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_03_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : " );
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i <= number; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum from 1 to " + number + " is " + sum);
+        }
+    }
+}
