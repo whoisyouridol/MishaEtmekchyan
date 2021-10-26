9f64759f66735216012bb681c13d6660bba23996
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-26 20:39:14 +0400

    finish task_6

---
+++ Day_03/Day_03_Task_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_03_Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Divisors of 36 are : ");
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0 && i < number)
+                    Console.Write(i + ", ");
+
+                if (i == number)
+                    Console.Write(i);        
+            }
+        }
+    }
+}
