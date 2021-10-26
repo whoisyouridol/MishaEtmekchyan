8413a1d7dfc320fa4803806d36f04bffafe41299
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-26 20:01:00 +0400

    finish task_1

---
+++ Day_03/Day_03_Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day_03_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Sum from 0 to 10 is " + sum);
+        }
+    }
+}
