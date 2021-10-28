819fedcf43dac0cb0ff8f555781c50c3d5898729
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-28 23:18:35 +0400

    finish task_5

---
+++ Day_04/Day_04_Task_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day_04_Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size : ");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            var arr = new int[arrSize];
+            for (int i = 0; i < arrSize; i++)
+            {
+                Console.Write("Enter number for index " + i + ": ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Unique elements of array are : ");
+            for (int i = 0; i < arrSize; i++)
+            {
+                bool isUnique = true;
+                for (int j = 0; j < arrSize; j++)
+                {
+                    if (arr[i] == arr[j] && i !=j)
+                    {
+                        isUnique = false;
+                    }
+                }
+                if (isUnique)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+        }
+    }
+}
