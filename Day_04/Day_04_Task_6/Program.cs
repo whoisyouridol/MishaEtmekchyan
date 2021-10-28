6965e72ef1ad3f92f1a4dd041a571ac20ffc8b58
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-28 23:39:29 +0400

    finish task_6

---
+++ Day_04/Day_04_Task_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day_04_Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array row size : ");
+            int rowSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter array column size : ");
+            int columnSize = Convert.ToInt32(Console.ReadLine());
+            var arr = new int[rowSize, columnSize];
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < columnSize; j++)
+                {
+                    Console.Write("Enter number for index " + i + "," + j + " : ");
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < columnSize; j++)
+                {
+                    if (j == columnSize - 1)
+                    {
+                        Console.Write(arr[i, j]);
+
+                    }
+                    else
+                        Console.Write(arr[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
