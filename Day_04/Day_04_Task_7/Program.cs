7c424a916a7f1805b3496ea9ed2885d28748d7a0
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-28 23:45:26 +0400

    finish task_7

--- Day_04/Day_04_Task_7/Program.cs
+++ Day_04/Day_04_Task_7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-namespace Day_04_Task_6
+
+namespace Day_04_Task_7
 {
     class Program
     {
@@ -10,7 +11,11 @@ namespace Day_04_Task_6
 
             Console.Write("Enter array column size : ");
             int columnSize = Convert.ToInt32(Console.ReadLine());
+
             var arr = new int[rowSize, columnSize];
+            var arr2 = new int[rowSize, columnSize];
+
+            Console.WriteLine("Fill first matrix : ");
             for (int i = 0; i < rowSize; i++)
             {
                 for (int j = 0; j < columnSize; j++)
@@ -19,17 +24,32 @@ namespace Day_04_Task_6
                     arr[i, j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
+
+            Console.WriteLine("Fill second matrix : ");
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < columnSize; j++)
+                {
+                    Console.Write("Enter number for index " + i + "," + j + " : ");
+                    arr2[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+
+
+            Console.WriteLine("Here is sum of two matrices");
             for (int i = 0; i < rowSize; i++)
             {
                 for (int j = 0; j < columnSize; j++)
                 {
                     if (j == columnSize - 1)
                     {
-                        Console.Write(arr[i, j]);
+                        Console.Write(arr[i, j] + arr2[i,j]);
 
                     }
                     else
-                        Console.Write(arr[i, j] + ", ");
+                        Console.Write(arr[i, j] + arr2[i,j] + ", ");
                 }
                 Console.WriteLine();
             }
