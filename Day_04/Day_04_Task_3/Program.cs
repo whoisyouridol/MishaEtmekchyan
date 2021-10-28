b50c43724f2b6f4becbf02d5f32578c1c7b28be0
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-28 23:09:50 +0400

    finish task_3

---
+++ Day_04/Day_04_Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_04_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size : ");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            var arr = new int[arrSize];
+            int sum = 0;
+            for (int i = 0; i < arrSize; i++)
+            {
+                Console.Write("Enter number for index " + i + ": ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i];
+            }
+            Console.WriteLine("Sum of array elements is : " + sum);
+        }
+    }
+}
