2f2ed2834d84dacf21fc8eb4327a1a12cf1d2b5c
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-10-31 12:40:35 +0400

    finish task_8

---
+++ Day_05/Day_05_Task_8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day_05_Task_8
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int num = 1230;
+            int power = 0;
+            string result = "";
+            while (num > 0)
+            {
+                int digit = num % 10;
+                if (num < 10)
+                    result += digit + " * " + "10^" + power;
+                else
+                    result += digit + " * " + "10^" + power + " + ";
+                num /= 10;
+                power++;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
