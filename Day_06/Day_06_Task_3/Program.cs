ec6eba2789a4d9870fd89de642f64bc3ce404853
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-11-02 20:19:50 +0400

    finish task_3

---
+++ Day_06/Day_06_Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_06_Task_3
+{
+    class Program
+    {
+        static string AddSpaces(string str)
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                result += str[i] + " ";
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddSpaces("Hello World!"));
+        }
+    }
+}
