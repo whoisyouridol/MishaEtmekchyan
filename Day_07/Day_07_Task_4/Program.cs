9f6b5cd6cc9e7eef5831629bdcd497923c880b6e
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-11-07 21:13:41 +0400

    finish task_4

---
+++ Day_07/Day_07_Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_07_Task_4
+{
+    class Program
+    {
+        static int CountPower(int num, int power)
+        {
+            if (power == 0)
+            {
+                return 1;
+            }
+            return num * CountPower(num, power - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountPower(2,10));
+        }
+    }
+}
