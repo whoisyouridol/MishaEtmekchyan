cd256d840386fd22d637345be8914b2b833fc9c1
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-11-11 23:04:17 +0400

    finish task_3 day_09

---
+++ Day_09/Day_09_Task_3/Clock.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day_09_Task_3
+{
+    class Clock
+    {
+        private int _Seconds;
+        public int Seconds
+        {
+            get { return _Seconds; }
+            set
+            {
+                if (value >= 0 && value <= 59)
+                {
+                    _Seconds = value;
+                }
+                else throw new InvalidDataException();
+            }
+        }
+
+        private int _Minutes;
+        public int Minutes
+        {
+            get { return _Minutes; }
+            set
+            {
+                if (value >= 0 && value <= 59)
+                {
+                    _Minutes = value;
+                }
+                else throw new InvalidDataException();
+            }
+        }
+
+        private int _Hours;
+        public int Hours
+        {
+            get { return _Hours; }
+            set
+            {
+                if (value >= 0 && value <= 23)
+                {
+                    _Hours = value;
+                }
+                else throw new InvalidDataException();
+            }
+        }
+        //public void AddSeconds()
+        //{
+        //    if (_Seconds < 59)
+        //    {
+        //        _Seconds++;
+        //    }
+        //    else if(Minutes < 59)
+        //    {
+        //        _Seconds = 0;
+        //        _Seconds++;
+        //        _Minutes++;
+        //    }
+        //    else if(Hours < 23)
+        //    {
+        //        _Seconds = 0;
+        //        _Minutes = 0;
+
+        //        _Seconds++;
+        //        _Minutes++;
+        //        _Hours++;
+        //    }
+        //    else 
+        //    {
+        //        _Seconds = 0;
+        //        _Minutes = 0;
+        //        _Hours = 0;
+        //    }
+        //}
+        //public void AddMinutes()
+        //{
+        //    if (_Minutes < 59)
+        //    {
+        //        _Minutes++;
+        //    }
+        //    else if (Hours < 23)
+        //    {
+        //        _Minutes = 0;
+        //        _Minutes++;
+        //        _Hours++;
+        //    }
+        //    else
+        //    {
+        //        _Seconds = 0;
+        //        _Minutes = 0;
+        //        _Minutes++;
+        //        _Seconds++;
+        //        _Hours = 0;
+        //    }
+        //}
+        //public void AddHours()
+        //{
+        //    if (_Hours < 23)
+        //    {
+        //        _Hours++;
+        //    }
+        //    else
+        //    {
+        //        _Seconds = 0;
+        //        _Minutes = 0;
+        //        _Hours = 0;
+        //    }
+        //}
+
+        public void AddHour()
+        {
+            time = time.AddHours(1);
+        }
+        public void AddMinute()
+        {
+            time = time.AddMinutes(1);
+        }
+        public void AddSecond()
+        {
+            time = time.AddSeconds(1);
+        }
+        private DateTime time;
+        public void SetTime()
+        {
+            time = DateTime.Parse($"{_Hours}:{_Minutes}:{Seconds}");
+        }
+        public void GetCurrentTime()
+        {
+            Console.WriteLine(time.ToString("HH:mm:ss"));
+        }
+    }
+}
---
+++ Day_09/Day_09_Task_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day_09_Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var clock = new Clock();
+            Console.Write("Enter hours : ");
+            clock.Hours = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter minutes : ");
+            clock.Minutes = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter seconds : ");
+            clock.Seconds = Convert.ToInt32(Console.ReadLine());
+            clock.SetTime();
+            clock.AddSecond();
+            clock.AddSecond();
+            clock.AddSecond();
+            clock.GetCurrentTime();
+
+
+        }
+    }
+}
