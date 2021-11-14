81d136ade6f46510d96523c0165535bc41b01ad6
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-11-14 13:46:19 +0400

    finish task

---
+++ Day_11/Day_11_Task/Statuses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_11_Task
+{
+    enum Statuses
+    {
+        Success,
+        PowMustBeaPositiveOrZero,
+        Undefined,
+        Equal
+    }
+}
