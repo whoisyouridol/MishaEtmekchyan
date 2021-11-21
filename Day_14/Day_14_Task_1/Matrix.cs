8ebf27676c39ed7803ef4871f92e83ebb8c6e204
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-11-21 18:58:18 +0400

    finish task_1

---
+++ Day_14/Day_14_Task_1/Matrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_14_Task_1
+{
+    public class Matrix
+    {
+        public double[,] Elements { get; set; }
+        public Matrix(double a, double b, double c, double d)
+        {
+            Elements = new double[,]
+              {
+                  {a,b },
+                  {c,d }
+              };
+        }
+        private static double[,] _getElements(Matrix matrix)
+        {
+            return new double[,]
+              {
+                { matrix.Elements[0,0],matrix.Elements[0,1] },
+                { matrix.Elements[1,0],matrix.Elements[1,1] }
+            };
+        }
+        public static implicit operator double (Matrix matrix)
+        {
+            return _getElements(matrix)[0, 0] * _getElements(matrix)[1, 1] - _getElements(matrix)[0, 1] * _getElements(matrix)[1,0] ;
+        }
+        public static Matrix operator * (Matrix m1, Matrix m2)
+        {
+            return new Matrix(
+                m1.Elements[0, 0] * m2.Elements[0,0] + m1.Elements[0,1] * m2.Elements[1,0],
+                m1.Elements[0, 0] * m2.Elements[0, 1] + m1.Elements[0, 1] * m2.Elements[1, 1],
+                m1.Elements[1, 0] * m2.Elements[0, 0] + m1.Elements[1, 1] * m2.Elements[1, 0],
+                m1.Elements[1, 0] * m2.Elements[0, 1] + m1.Elements[1, 1] * m2.Elements[1, 1]);
+        }
+        public static Matrix operator -(Matrix matrix)
+        {
+            double det = matrix;
+            var adjMatrix = new Matrix(
+                matrix.Elements[1, 1], -matrix.Elements[0, 1], 
+                -matrix.Elements[1, 0], matrix.Elements[0, 0]);
+            return new Matrix(
+                1/det * adjMatrix.Elements[0, 0],
+                1/det * adjMatrix.Elements[0, 1],
+                1/det * adjMatrix.Elements[1, 0],
+                1/det * adjMatrix.Elements[1, 1]);
+        }
+        public override string ToString()
+        {
+            return $"{Elements[0,0]}    {Elements[0,1]}\n" +
+                $"{Elements[1,0]}   {Elements[1,1]}";
+        }
+        public override bool Equals(object matrix)
+        {
+            var converted = (Matrix)matrix;
+            return converted.Elements[0, 0] == Elements[0, 0] &&
+                   converted.Elements[0, 1] == Elements[0, 1] &&
+                   converted.Elements[1, 0] == Elements[1, 0] &&
+                   converted.Elements[1, 1] == Elements[1, 1];
+        }
+    }
+}
---
+++ Day_14/Day_14_Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day_14_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m1 = new Matrix(1, 2, 3, 4);
+            Matrix m2 = new Matrix(1, 4, 5, 2);
+
+            var m3 = m1 * m2;
+            Console.WriteLine(m3.ToString());
+            Console.WriteLine("_________________________________________");
+            var m4 = -m1;
+            Console.WriteLine(m4.ToString());
+
+            Console.WriteLine(m4.Equals(m1));
+        }
+    }
+}
