a116ee10cd6d01bba6a942ae3605ebc4c66f8195
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-12-01 18:20:13 +0400

    finish task_1

---
+++ Day_17/Day_17_Task_1/CreditIBAN.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_17_Task_1
+{
+    public class CreditIBAN : IBAN
+    {
+        public decimal CurrentCredit { get; set; }
+        public DateTime PaymentDeadLine { get; set; }
+    }
+}
---
+++ Day_17/Day_17_Task_1/IBAN.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_17_Task_1
+{
+    public class IBAN
+    {
+        public string BankAccount { get; set; }
+        public decimal Amount { get; set; }
+        public override string ToString()
+        {
+            return $"Bank account : {BankAccount}, amount on this account : {Amount}";
+        }
+        public void AddAmount(decimal a)
+        {
+            Amount += a; 
+        }
+        public void GetAmount(decimal a)
+        {
+            Amount -= a;
+        }
+    }
+}
