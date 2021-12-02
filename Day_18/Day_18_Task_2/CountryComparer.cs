7e364f03a25ecf248e586b81eaab8e5ef2f46b36
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-12-02 22:22:59 +0400

    finish task_2

---
+++ Day_18/Day_18_Task_2/CountryComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Day_18_Task_2
+{
+    class CountryComparer : IEqualityComparer<Country>
+    {
+        public bool Equals( Country x,  Country y)
+        {
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode( Country obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
---
+++ Day_18/Day_18_Task_2/GeographicEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_18_Task_2
+{
+    public abstract class GeographicEntity
+    {
+        public double Area { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
+}
