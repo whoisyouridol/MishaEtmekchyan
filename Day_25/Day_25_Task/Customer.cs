00bf731bcc8ae78a6127c01efc859626ac5648d5
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-12-19 21:36:18 +0400

    finish task from Day_25

---
+++ Day_25/Day_25_Task/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_25_Task
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
