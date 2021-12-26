5b47cbdec5b297104930a308dcbc25031eb28608
mikheil.etmekchyan882@ens.tsu.edu.ge <mikheil.etmekchyan882@ens.tsu.edu.ge>
2021-12-26 15:40:38 +0400

    finish task_1

---
+++ Day_28/Task_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Task_1
+{
+    class Program
+    {
+        private async static Task WriteToFile(int i, CancellationToken token)
+        {
+            var path = @$"C:\Users\Admin Admin\source\repos\MishaEtmekchyan\Day_28\Task_1\{i}.txt";
+            File.Create(path).Close();
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(100 * i);
+                await File.AppendAllTextAsync(path, $"Task{i} ");
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            var tokenSrc = new CancellationTokenSource();
+            List<Task> tasks = new List<Task>(10);
+            tasks.Add(Task.Run(() => 
+            {
+                Console.WriteLine("Stop ?");
+                var ans = Console.ReadLine();
+                if (ans == "stop")
+                {
+                    tokenSrc.Cancel();
+                }
+            }));
+            for (int i = 1; i < 10; i++)
+            {
+                tasks.Add(Task.Run(() => WriteToFile(i, tokenSrc.Token)));
+            }
+
+            await Task.WhenAll(tasks);
+
+
+        }
+    }
+}
